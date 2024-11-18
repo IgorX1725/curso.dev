106fb68411a3b281c9fd345fa0aad68f5c334df8
Igor Cortez <ibarbos2@its.jnj.com>
2024-11-18 10:18:02 -0300

    some refactorings

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,6 @@
 const Home = ()=>{
-return (<h1>You Rock! I admire your resilience.</h1>)
+    const homeMessage = process.env.NEXT_PUBLIC_MESSAGE
+return <h1>{homeMessage}</h1>
 }
+
+export default Home
