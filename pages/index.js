17b1f46980dbdba36450008db9993472fe4c8b86
Igor Cortez <ibarbos2@its.jnj.com>
2024-11-28 16:05:34 -0300

    Add scripts `lint:check` and `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
-const Home = ()=>{
-  const homeMessage = process.env.NEXT_PUBLIC_MESSAGE
-return <h1>{homeMessage}</h1>
-}
+const Home = () => {
+  const homeMessage = process.env.NEXT_PUBLIC_MESSAGE;
+  return <h1>{homeMessage}</h1>;
+};
 
-export default Home
+export default Home;
