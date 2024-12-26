fb4df49cc76bfd2ba722ec6da8ae3f96aa66ad60
Igor Cortez <igorcortez@Igors-Air.home>
2024-12-26 14:58:14 -0300

    Finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,28 @@
 import database from "infra/database";
 
 const status = async (req, res) => {
-  const result = await database.query("SELECT 1 + 1 as sum");
-  console.log(result.rows);
-  res.status(200).json({ message: "status OK" });
+  const updatedAt = new Date().toISOString();
+  const databaseVersion = await database.query("SHOW server_version;");
+  const databaseMaxConnections = await database.query("SHOW max_connections;");
+
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseUsedConnections = await database.query({
+    text: `SELECT COUNT(*)::int AS used_connections FROM pg_stat_activity WHERE datname = $1;`,
+    values: [databaseName],
+  });
+
+  res.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersion.rows[0].server_version,
+        max_connections: parseInt(
+          databaseMaxConnections.rows[0].max_connections,
+        ),
+        used_connections: databaseUsedConnections.rows[0].used_connections,
+      },
+    },
+  });
 };
 
 export default status;
