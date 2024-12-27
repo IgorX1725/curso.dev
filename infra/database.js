5875f7c785b1157c75c78b9143826dc3ef336e1d
Igor Cortez <igorcortez@Igors-Air.home>
2024-12-27 15:00:18 -0300

    Add more robuts SSL configuration method

--- infra/database.js
+++ infra/database.js
@@ -10,15 +10,6 @@ const query = async (queryObject) => {
     ssl: process.env.NODE_ENV === "production",
   });
 
-  console.log("Postgres Credentials", {
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
-    ssl: process.env.NODE_ENV === "production",
-  });
-
   try {
     await client.connect();
 
@@ -33,4 +24,12 @@ const query = async (queryObject) => {
 
 export default {
   query: query,
+};
+
+const getSSLValues = () => {
+  if (process.env.POSTGRES_CA) {
+    return process.env.POSTGRES_CA;
+  }
+
+  return process.env.NODE_ENV === "production";
 };
