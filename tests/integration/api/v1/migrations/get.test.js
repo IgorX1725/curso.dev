e756ccad69c1fba6e507ad377a987ce23af28851
Igor Cortez <igorcortez@Igors-Air.home>
2025-01-15 21:50:40 -0300

    Makes `npm test` more rebust with `async-retry` and `orchestrator`

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -1,18 +1,17 @@
-import database from 'infra/database'
+import database from "infra/database";
+import orchestrator from "tests/orchestrator";
 
-const cleanDatabase = async ()=>{
-  await database.query('DROP schema public cascade; create schema public;')
-}
-
-beforeAll(cleanDatabase)
+beforeAll(async () => {
+  await orchestrator.waitForAllServices();
+  await database.query("DROP schema public cascade; create schema public;");
+});
 
 test("GET to /api/v1/migrations should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/migrations");
   expect(response.status).toBe(200);
 
-  const responseBody = await response.json()
+  const responseBody = await response.json();
 
-  expect(Array.isArray(responseBody)).toBe(true)
-  expect(responseBody.length).toBeGreaterThan(0)
-
+  expect(Array.isArray(responseBody)).toBe(true);
+  expect(responseBody.length).toBeGreaterThan(0);
 });
--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -1,28 +1,29 @@
-import database from 'infra/database'
+import database from "infra/database";
+import orchestrator from "tests/orchestrator";
 
-const cleanDatabase = async ()=>{
-  await database.query('DROP schema public cascade; create schema public;')
-}
-
-beforeAll(cleanDatabase)
+beforeAll(async () => {
+  await orchestrator.waitForAllServices();
+  await database.query("DROP schema public cascade; create schema public;");
+});
 
 test("GET to /api/v1/migrations should return 200", async () => {
-  const migrationRequest = ()=> fetch("http://localhost:3000/api/v1/migrations", { method: 'POST'});
-  const responseBeforeMigrationsToBeRun = await migrationRequest()
+  const migrationRequest = () =>
+    fetch("http://localhost:3000/api/v1/migrations", { method: "POST" });
+  const responseBeforeMigrationsToBeRun = await migrationRequest();
   expect(responseBeforeMigrationsToBeRun.status).toBe(201);
 
-  const responseBodyBeforeMigrationsToBeRun = await responseBeforeMigrationsToBeRun.json()
+  const responseBodyBeforeMigrationsToBeRun =
+    await responseBeforeMigrationsToBeRun.json();
 
-  expect(Array.isArray(responseBodyBeforeMigrationsToBeRun)).toBe(true)
-  expect(responseBodyBeforeMigrationsToBeRun.length).toBeGreaterThan(0)
+  expect(Array.isArray(responseBodyBeforeMigrationsToBeRun)).toBe(true);
+  expect(responseBodyBeforeMigrationsToBeRun.length).toBeGreaterThan(0);
 
-
-
-  const responseAfterMigrationsToBeRun = await migrationRequest()
+  const responseAfterMigrationsToBeRun = await migrationRequest();
   expect(responseAfterMigrationsToBeRun.status).toBe(200);
 
-  const responseBodyAfterMigrationsToBeRun = await responseAfterMigrationsToBeRun.json()
+  const responseBodyAfterMigrationsToBeRun =
+    await responseAfterMigrationsToBeRun.json();
 
-  expect(Array.isArray(responseBodyAfterMigrationsToBeRun)).toBe(true)
-  expect(responseBodyAfterMigrationsToBeRun.length).toEqual(0)
+  expect(Array.isArray(responseBodyAfterMigrationsToBeRun)).toBe(true);
+  expect(responseBodyAfterMigrationsToBeRun.length).toEqual(0);
 });
