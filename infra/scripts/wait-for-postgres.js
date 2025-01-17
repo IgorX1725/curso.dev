1a81e5aa55fddb83131b1473265d543b64fac3af
Igor Cortez <igorcortez@Igors-Air.home>
2025-01-16 22:55:28 -0300

    Fix `Prettier` style linting

--- infra/scripts/wait-for-postgres.js
+++ infra/scripts/wait-for-postgres.js
@@ -1,20 +1,18 @@
-const { exec } = require("node:child_process")
+const { exec } = require("node:child_process");
 
 const handleReturn = (_, stdout) => {
   if (stdout.search("accepting connections") === -1) {
-    process.stdout.write(".")
+    process.stdout.write(".");
 
-    setTimeout(checkPostgres, 1000)
-    return
+    setTimeout(checkPostgres, 1000);
+    return;
   }
-  console.log('\n🟢 Postgres is ready and accepting connections!\n')
-}
+  console.log("\n🟢 Postgres is ready and accepting connections!\n");
+};
 
 const checkPostgres = () => {
-  exec("docker exec postgres-dev pg_isready --host localhost", handleReturn)
+  exec("docker exec postgres-dev pg_isready --host localhost", handleReturn);
+};
 
-}
-
-process.stdout.write('\n\n🔴 Waiting for Postgres to accept for connections')
-checkPostgres()
-
+process.stdout.write("\n\n🔴 Waiting for Postgres to accept for connections");
+checkPostgres();
