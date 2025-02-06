cb522d7169488c7b030e16d38cb0949b40734c86
Igor Cortez <igorcortez@Igors-Air.home>
2025-02-05 23:40:46 -0300

    refactor: update eslint configuration to newest flat system

---
+++ eslint.config.mjs
@@ -0,0 +1,10 @@
+import js from "@eslint/js";
+import jest from "eslint-plugin-jest";
+import prettier from "eslint-config-prettier";
+
+export default [
+  js.configs.recommended,
+  {
+    plugins: { jest, prettier }
+  },
+];
